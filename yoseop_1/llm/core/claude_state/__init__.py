4195f630acd5e5004d26dae5506ccf766edca22b
swwon-i <stonesw123@naver.com>
2025-07-29 14:49:21 +0900

    delete llm/core

--- yoseop_1/llm/core/claude_state/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Claude 세션 상태 관리 모듈
-"""
-
-from .session_manager import SessionStateManager, create_session_manager, check_and_restore_session
-
-__all__ = ['SessionStateManager', 'create_session_manager', 'check_and_restore_session']
